c6f070409c0afbce708e46b14dfedbe8768975a0
Bhavsay Potan <bhavsaypotan@gmail.com>
2025-07-24 21:18:53 +0530

    Time: 0 ms (100%), Space: 46.2 MB (7.44%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.java
@@ -0,0 +1,22 @@
+class Solution {
+    // Return the index
+    // Return -1 if it does not exist
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
