8268369a252c1b6de5c4d763ae1870de13624725
Bhavsay Potan <bhavsaypotan@gmail.com>
2025-07-27 07:05:41 +0530

    Time: 0 ms (100%), Space: 42.1 MB (97.81%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findPeakElement(int[] arr) {
+         int n = arr.length;
+        
+        for (int i=0; i<n; i++) {
+            boolean leftCheck = (i == 0 || arr[i] > arr[i - 1]);
+            boolean rightCheck = (i == n - 1 || arr[i] > arr[i + 1]);
+
+            if (leftCheck && rightCheck) {
+                return i;  // Return index of any one peak
+            }
+        }
+        return -1;
+        
+    }
+}
