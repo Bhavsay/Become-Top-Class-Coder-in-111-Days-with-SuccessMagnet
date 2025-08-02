e5651bb7234e44bbd490d1f2e0aee720fd4add97
Bhavsay Potan <bhavsaypotan@gmail.com>
2025-08-02 07:47:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Minimum distance in an Array/minimum-distance-in-an-array.java
+++ Difficulty: Easy/Minimum distance in an Array/minimum-distance-in-an-array.java
@@ -1,15 +1,35 @@
+// class Solution {
+//     int minDist(int arr[], int x, int y) {
+//         int minDist = Integer.MAX_VALUE;
+//         int prev = -1;
+//         for (int i = 0; i < arr.length; i++) {
+//             if (arr[i] == x || arr[i] == y) {
+//                 if (prev != -1 && arr[i] != arr[prev]) {
+//                     minDist = Math.min(minDist, i - prev);
+//                 }
+//                 prev = i;
+//             }
+//         }
+//         return (minDist == Integer.MAX_VALUE) ? -1 : minDist;
+//     }
+// }
+import java.util.*;
+
 class Solution {
-    int minDist(int arr[], int x, int y) {
-        int minDist = Integer.MAX_VALUE;
-        int prev = -1;
+    public int minDist(int[] arr, int x, int y) {
+        int min = Integer.MAX_VALUE;
+        HashMap<Integer, Integer> map = new HashMap<>(); // stores last index of x or y
+        
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == x || arr[i] == y) {
-                if (prev != -1 && arr[i] != arr[prev]) {
-                    minDist = Math.min(minDist, i - prev);
+                if (map.containsKey(arr[i] == x ? y : x)) { 
+                    int lastIndex = map.get(arr[i] == x ? y : x);
+                    min = Math.min(min, i - lastIndex);
                 }
-                prev = i;
+                map.put(arr[i], i); // update last index of current number
             }
         }
-        return (minDist == Integer.MAX_VALUE) ? -1 : minDist;
+        
+        return (min == Integer.MAX_VALUE) ? -1 : min;
     }
 }
