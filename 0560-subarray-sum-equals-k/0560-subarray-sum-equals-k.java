632baa512dd6cca2dd806951fc1241f00b57febb
Bhavsay Potan <bhavsaypotan@gmail.com>
2025-08-06 18:38:22 +0530

    Time: 18 ms (99.64%), Space: 47.6 MB (5.85%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,17 +1,53 @@
 class Solution {
-    public int subarraySum(int[] nums, int k) {
-        int count = 0;
-        int n = nums.length;
-
-        for (int i = 0; i < n; i++) {
-            int sum = 0; // reset sum for each starting index
-            for (int j = i; j < n; j++) {
-                sum += nums[j];
-                if (sum == k) {
-                    count++;
-                }
+    public int subarraySum(int[] arr, int k){
+        int n = arr.length;
+        int pref[] = new int[n];
+        pref[0]=arr[0];
+        for(int i=1; i<n; i++){
+            pref[i]=pref[i-1]+arr[i];
+        }
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
+        int count =-0;
+        for(int i=0; i<n; i++){
+            if(map.containsKey(pref[i]-k)){
+                count+=map.get(pref[i]-k);
             }
+            map.put(pref[i],map.getOrDefault(pref[i],0)+1);
         }
         return count;
     }
 }
+// class Solution {
+//     public int subarraySum(int[] nums, int k) {
+//         int count = 0;
+//         int n = nums.length;
+
+//         for (int i = 0; i < n; i++) {
+//             int sum = 0; // reset sum for each starting index
+//             for (int j = i; j < n; j++) {
+//                 sum += nums[j];
+//                 if (sum == k) {
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// }
+/*
+Qfind the number of subarrays whose sum == k 
+subarray :- It is contineous portion or part of array
+brute force approach 
+Optimize :- o(n)
+HashMap<Integer, Integer> map = new HashMap<>();
+map.put(0,1)
+int count =0;
+for(int i=0; i<n; i++){
+    if(map.containsKey(pref[i]-k)){
+        count+=map.get(pref[i]-k);
+    } 
+    map.put(pref[i],map.geOrDefault(pref[i],0)+1);
+    } 
+    return count;
+}*/
