8f86d0a1de124dd986c8175e6717cd9785c35f38
Bhavsay Potan <bhavsaypotan@gmail.com>
2025-08-08 21:36:36 +0530

    Time: 13 ms (29.33%), Space: 44.9 MB (16.94%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,24 +1,46 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
+        if (s.length() != t.length()) return false; // quick length check
+
+        Map<Character, Integer> ma = new HashMap<>();
+        Map<Character, Integer> mb = new HashMap<>();
+
+        for (char ch : s.toCharArray()) {
+            ma.put(ch, ma.getOrDefault(ch, 0) + 1);
+        }
+        for (char ch : t.toCharArray()) {
+            mb.put(ch, mb.getOrDefault(ch, 0) + 1);
         }
 
-        Map<Character, Integer> counter = new HashMap<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            counter.put(ch, counter.getOrDefault(ch, 0) + 1);
-        }
-
-        for (int i = 0; i < t.length(); i++) {
-            char ch = t.charAt(i);
-            if (!counter.containsKey(ch) || counter.get(ch) == 0) {
-                return false;
-            }
-            counter.put(ch, counter.get(ch) - 1);
-        }
-
-        return true;        
+        return ma.equals(mb);
     }
 }
+
+
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         if (s.length() != t.length()) {
+//             return false;
+//         }
+
+//         Map<Character, Integer> counter = new HashMap<>();
+
+//         for (int i = 0; i < s.length(); i++) {
+//             char ch = s.charAt(i);
+//             counter.put(ch, counter.getOrDefault(ch, 0) + 1);
+//         }
+
+//         for (int i = 0; i < t.length(); i++) {
+//             char ch = t.charAt(i);
+//             if (!counter.containsKey(ch) || counter.get(ch) == 0) {
+//                 return false;
+//             }
+//             counter.put(ch, counter.get(ch) - 1);
+//         }
+
+//         return true;        
+//     }
+// }
