736ff556243cc72d9debafc98b5ee2d2b10e324a
Bhavsay Potan <bhavsaypotan@gmail.com>
2025-08-21 20:02:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Largest Element in Array/largest-element-in-array.java
+++ Difficulty: Basic/Largest Element in Array/largest-element-in-array.java
@@ -1,9 +1,18 @@
 class Solution {
     public static int largest(int[] arr) {
-        int max=arr[0];
+        
+        int max = arr[0];
+        
         for(int i=1; i<arr.length; i++){
+            
             if(arr[i]>max){
-                max=arr[i];
+                
+                max = arr[i];
+            }
+            else if(max>arr[i]){
+                
+                arr[i]=max;
+                
             }
         }
         return max;
