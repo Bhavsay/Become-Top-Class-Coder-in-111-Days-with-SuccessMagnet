e5228ace91532eadcad2ab5aeee4c746b0700f73
Bhavsay Potan <bhavsaypotan@gmail.com>
2025-08-28 22:21:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse Words/reverse-words.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String reverseWords(String s) {
+        int n = s.length();
+        StringBuilder ans = new StringBuilder();
+
+        StringBuilder sb = new StringBuilder(s);
+        sb.reverse();
+
+        int i = 0;
+        while (i < n) {
+
+            while (i < n && sb.charAt(i) == '.') i++;
+
+            
+            StringBuilder word = new StringBuilder();
+            while (i < n && sb.charAt(i) != '.') {
+                word.append(sb.charAt(i));
+                i++;
+            }
+
+            
+            if (word.length() > 0) {
+                if (ans.length() > 0) ans.append(".");
+                ans.append(word.reverse());
+            }
+        }
+
+        return ans.toString();
+        
+    }
+}
