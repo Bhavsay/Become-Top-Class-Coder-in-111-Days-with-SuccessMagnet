ec24a73a024993a88a3493c1afabb967741dd2d7
Bhavsay Potan <bhavsaypotan@gmail.com>
2025-08-31 21:16:24 +0530

    Time: 38 ms (11.38%), Space: 46.3 MB (12.36%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        int n = candyType.length;
+        int canEat = n/2;
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0; i<n; i++){
+            map.put(candyType[i],map.getOrDefault(candyType[i],0)+1);
+        }
+        return Math.min(canEat,map.size());
+        
+    }
+}
