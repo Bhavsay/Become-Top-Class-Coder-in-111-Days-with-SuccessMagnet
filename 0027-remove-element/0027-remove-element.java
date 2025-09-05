dee78f78744217dcbe35c6df224c09c06ef0f297
Bhavsay Potan <bhavsaypotan@gmail.com>
2025-09-05 12:05:03 +0530

    Time: 0 ms (100%), Space: 42 MB (68.3%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,17 +1,60 @@
 class Solution {
-    public int removeElement(int[] nums, int val) {
-
-        // for(int i=nums.length-1; i>=0; i--){
-        //     if(arr[i])
-        // }
-        int k = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                nums[k] = nums[i];
+    public int removeElement(int[] nums,int val){
+        int k=0;
+        
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] !=val){
+                nums[k]=nums[i];
                 k++;
             }
         }
-        return k;  
+        return k; 
     }
 }
+
+
+
+// class Solution {
+//      public static void swap(int arr[],int start, int end) {
+//         int temp = 0;
+//         temp = arr[start];
+//         arr[start] = arr[end];
+//         arr[end] = temp;
+//     }
+//     public int removeElement(int[] nums, int val) {
+//         int i = 0, j = nums.length - 1;
+//         while (i <= j){
+//             if (nums[i] == val) {
+//                 if (nums[j] == val) {
+//                     j--;
+//                 }else {
+//                     swap(nums, i, j);
+//                     j--;
+//                     i++;
+//                 }
+//             } else {
+//                 i++;
+//             }
+//         }
+//         return i;
+//     }
+// }
+
+
+// class Solution {
+//     public int removeElement(int[] nums, int val) {
+
+//         // for(int i=nums.length-1; i>=0; i--){
+//         //     if(arr[i])
+//         // }
+//         int k = 0;
+
+//         for (int i = 0; i < nums.length; i++) {
+//             if (nums[i] != val) {
+//                 nums[k] = nums[i];
+//                 k++;
+//             }
+//         }
+//         return k;  
+//     }
+// }
