a52e1c887c2f7973745ff15d1cb0f7920193b82e
Bhavsay Potan <bhavsaypotan@gmail.com>
2025-09-10 23:27:26 +0530

    Time: 1 ms (99.46%), Space: 58.1 MB (69.15%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,21 +1,44 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
+        int start = 0;
+        int sum = 0;
         int minLen = Integer.MAX_VALUE;
-        int left = 0;
-        int curSum = 0;
 
-        for (int right = 0; right < nums.length; right++) {
-            curSum += nums[right];
-
-            while (curSum >= target) {
-                if (right - left + 1 < minLen) {
-                    minLen = right - left + 1;
-                }
-                curSum -= nums[left];
-                left++;
+        for (int end = 0; end < nums.length; end++) {
+            sum += nums[end];
+            while (sum >= target) {
+                minLen = Math.min(minLen, end - start + 1);
+                sum -= nums[start];
+                start++;
             }
         }
-        
-        return minLen != Integer.MAX_VALUE ? minLen : 0;        
+
+       if (minLen == Integer.MAX_VALUE) {
+         return 0;
+        } else {
+            return minLen;
+    }
+
     }
 }
+// class Solution {
+//     public int minSubArrayLen(int target, int[] nums) {
+//         int minLen = Integer.MAX_VALUE;
+//         int left = 0;
+//         int curSum = 0;
+
+//         for (int right = 0; right < nums.length; right++) {
+//             curSum += nums[right];
+
+//             while (curSum >= target) {
+//                 if (right - left + 1 < minLen) {
+//                     minLen = right - left + 1;
+//                 }
+//                 curSum -= nums[left];
+//                 left++;
+//             }
+//         }
+        
+//         return minLen != Integer.MAX_VALUE ? minLen : 0;        
+//     }
+// }
