9d045981a51b5e9f9ae8c889210daa53fda6f4c2
Bhavsay Potan <bhavsaypotan@gmail.com>
2025-09-13 23:11:07 +0530

    Time: 0 ms (100%), Space: 44.8 MB (62.34%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -10,3 +10,15 @@ class Solution {
         return ++j;
     }
 }
+// class Solution {
+//     public int removeDuplicates(int[] nums) {
+//         int j = 0;
+//         for (int i = 1; i < nums.length; i++) {
+//             if (nums[j] != nums[i]) {
+//                 nums[++j] = nums[i];
+                
+//             }
+//         }
+//         return ++j;
+//     }
+// }
