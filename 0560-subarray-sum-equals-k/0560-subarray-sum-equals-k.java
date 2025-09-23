11c844c062f29a8911862c9ed9790ca2a93d09c5
Bhavsay Potan <bhavsaypotan@gmail.com>
2025-09-23 21:21:54 +0530

    Time: 24 ms (72.33%), Space: 46.5 MB (65.85%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,3 +1,21 @@
+public class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0, sum = 0;
+        HashMap < Integer, Integer > map = new HashMap < > ();
+        
+      
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if(sum == k) count++;
+            if (map.containsKey(sum - k))
+                count += map.get(sum - k);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+}
+
+/*
 class Solution {
     public int subarraySum(int[] arr, int k){
         int n = arr.length;
@@ -18,11 +36,11 @@ class Solution {
         return count;
     }
 }
+
 // class Solution {
 //     public int subarraySum(int[] nums, int k) {
 //         int count = 0;
 //         int n = nums.length;
-
 //         for (int i = 0; i < n; i++) {
 //             int sum = 0; // reset sum for each starting index
 //             for (int j = i; j < n; j++) {
@@ -35,11 +53,13 @@ class Solution {
 //         return count;
 //     }
 // }
-/*
+
+
 Qfind the number of subarrays whose sum == k 
 subarray :- It is contineous portion or part of array
 brute force approach 
 Optimize :- o(n)
+
 HashMap<Integer, Integer> map = new HashMap<>();
 map.put(0,1)
 int count =0;
