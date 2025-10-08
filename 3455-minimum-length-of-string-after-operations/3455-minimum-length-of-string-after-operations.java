31e1aa0aa8305b5f1c3bb69cf782570f2891f4dd
Bhavsay Potan <bhavsaypotan@gmail.com>
2025-10-08 21:56:54 +0530

    Time: 85 ms (36.28%), Space: 50.6 MB (21.53%) - LeetHub

---
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+class Solution {
+    public int minimumLength(String s) {
+        if(s.length()<=2){
+            return s.length();
+        }
+        int count = 0;
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(char c : s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        for(Map.Entry<Character,Integer> entry : map.entrySet()){
+            if(entry.getValue()%2==0){
+                count+=2;
+            }
+            else{
+                count+=1;
+            }
+        }    
+        return count;    
+    }
+}
