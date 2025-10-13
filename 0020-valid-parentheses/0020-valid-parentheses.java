9ca51fcd3b4fc0d628772906655dca2ce4a8907d
Bhavsay Potan <bhavsaypotan@gmail.com>
2025-10-13 21:14:28 +0530

    Time: 5 ms (10.74%), Space: 42.9 MB (5.25%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,27 @@
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if ("({[".contains(String.valueOf(s.charAt(i)))) {
+                stack.push(s.charAt(i));
+            } else {
+                if (!stack.isEmpty()) {
+
+                    if ((stack.peek() == '(' && s.charAt(i) == ')')
+                        || (stack.peek() == '{' && s.charAt(i) == '}')
+                        || (stack.peek() == '[' && s.charAt(i) == ']')) {
+                        stack.pop();
+                    } else {
+                        stack.push(s.charAt(i));
+                    }
+                } else {
+                    stack.push(s.charAt(i));
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
