f755203eab8d136a39dcfbe2fe1260aaeba3a807
Bhavsay Potan <bhavsaypotan@gmail.com>
2025-10-14 21:56:25 +0530

    Time: 477 ms (18.56%), Space: 63.2 MB (14.63%) - LeetHub

---
+++ 2751-sliding-subarray-beauty/2751-sliding-subarray-beauty.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int[] getSubarrayBeauty(int[] nums, int k, int x) {
+        int n = nums.length;
+        int[] res = new int[n-k+1];
+        int p=0;
+        Map<Integer,Integer> hm = new TreeMap<>();
+        int l=0;
+        int r=0;
+        while(r<n){
+            if(nums[r]<0) hm.put(nums[r],hm.getOrDefault(nums[r],0)+1);
+            if(r-l+1>k){
+                if(nums[l]<0){
+                    hm.put(nums[l],hm.get(nums[l])-1);
+                    if(hm.get(nums[l])==0)hm.remove(nums[l]);
+                }
+                l++;
+            }
+            if(r-l+1==k){
+                int count =0;
+                boolean flag= false;
+                for(Map.Entry<Integer,Integer> it:hm.entrySet()){
+                    count+=it.getValue();
+                    if(count>=x){
+                        res[p]=it.getKey();
+                        p++;
+                        flag=true;
+                        break;
+                    }
+                }
+                if(!flag)res[p++]=0;
+            }
+            r++;
+        }
+        return res;
+    }
+}
+
+    // // Test the code
+    // public static void main(String[] args) {
+    //     Solution s = new Solution();
+    //     int[] nums = {1, -1, -3, -2, 3, -2};
+    //     int k = 3, x = 2;
+    //     int[] res = s.getSubarrayBeauty(nums, k, x);
+    //     System.out.println(Arrays.toString(res));
+    // }
+// }
