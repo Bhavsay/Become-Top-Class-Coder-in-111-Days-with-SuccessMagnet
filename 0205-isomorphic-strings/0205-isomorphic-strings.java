22bb2b755ea5d0561cc217133b48f9004db1d43e
Bhavsay Potan <bhavsaypotan@gmail.com>
2025-10-18 22:35:47 +0530

    Time: 19 ms (19.29%), Space: 42.5 MB (54.73%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,25 +1,22 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if (s.length() != t.length()) return false;
-
-        char[] mapST = new char[128]; // mapping from s to t
-        char[] mapTS = new char[128]; // mapping from t to s
+        HashMap<Character, Integer> charIndexS = new HashMap<>();
+        HashMap<Character, Integer> charIndexT = new HashMap<>();
 
         for (int i = 0; i < s.length(); i++) {
-            char c1 = s.charAt(i);
-            char c2 = t.charAt(i);
+            if (!charIndexS.containsKey(s.charAt(i))) {
+                charIndexS.put(s.charAt(i), i);
+            }
 
-            if (mapST[c1] == 0 && mapTS[c2] == 0) {
-                // create mapping
-                mapST[c1] = c2;
-                mapTS[c2] = c1;
-            } else {
-                // check existing mapping
-                if (mapST[c1] != c2 || mapTS[c2] != c1) {
-                    return false;
-                }
+            if (!charIndexT.containsKey(t.charAt(i))) {
+                charIndexT.put(t.charAt(i), i);
+            }
+
+            if (!charIndexS.get(s.charAt(i)).equals(charIndexT.get(t.charAt(i)))) {
+                return false;
             }
         }
-        return true;
+
+        return true;        
     }
 }
