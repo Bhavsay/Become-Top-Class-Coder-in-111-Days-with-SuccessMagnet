f98d3dfeb486ebc0006f850d9f9a315e52c8e34c
Bhavsay Potan <bhavsaypotan@gmail.com>
2025-12-02 17:05:10 +0530

    Time: 2 ms (97.38%), Space: 43.3 MB (26.7%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,27 +1,33 @@
+import java.util.Stack;
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        for(char ch : s.toCharArray()){
-            if(ch=='(' || ch== '[' || ch== '{'){
+         Stack<Character> stack = new Stack<>();
+        char[] str = s.toCharArray();
+
+        for(char ch : str){
+            // opening brackets
+            if(ch == '(' || ch == '[' || ch == '{'){
                 stack.push(ch);
             }
             else {
-                if(stack.isEmpty()){
-                    return false;
+                // closing but nothing to match
+                if(stack.isEmpty()) return false;
+
+                if(ch == ')' && stack.peek() == '('){
+                    stack.pop();
                 }
-                char top = stack.pop();
-                if(ch == ')' && top != '('){
-                    return false;
+                else if(ch == ']' && stack.peek() == '['){
+                    stack.pop();
                 }
-                if(ch == ']' && top != '[') {
-                    return false;
+                else if(ch == '}' && stack.peek() == '{'){
+                    stack.pop();
                 }
-                if(ch == '}' && top != '{'){
+                else {
                     return false;
                 }
             }
         }
+
         return stack.isEmpty();
-        
     }
 }
