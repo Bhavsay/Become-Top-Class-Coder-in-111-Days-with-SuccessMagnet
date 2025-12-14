19a77340b60a828ddf5fcb7d91174f37bf5d2159
Bhavsay Potan <bhavsaypotan@gmail.com>
2025-12-14 07:42:28 +0530

    Time: 14 ms (28.01%), Space: 46.3 MB (29.07%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -19,3 +19,40 @@ class Solution {
         return map1.equals(map2);
     }
 }
+/*
+1️⃣ map.put(key, value)
+put() adds or updates key value in the map.
+HashMap<Character, Integer> map = new HashMap<>();
+map.put('a', 1);  map after execution { a = 1 }
+🔁 If key already exists
+map.put('a', 2);  map becomes { a = 2 } old value is replaced
+
+2️⃣ map.getOrDefault(key, defaultValue)
+👉 What it does
+If key exists → return its value
+If key does NOT exist → return defaultValue
+map.getOrDefault(key, defaultValue); syntax
+
+eg
+map.getOrDefault('b', 0);
+
+Explanation
+'b' not in map
+returns 0
+✔ No error, safe method
+
+
+🔹 3️⃣ Why We Use BOTH Together
+Correct Counting Pattern
+Syntax - map.put(ch, map.getOrDefault(ch, 0) + 1);
+s = "anagram"
+ch = 'a'
+map.getOrDefault('a', 0) → 0
+map.put('a', 0 + 1)
+{ a = 1 }
+{ a = 3, n = 1, g = 1, r = 1, m = 1 }
+*/
+
+
+
+
