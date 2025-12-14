00673341d471cf1c5d3c5f7f089c7cd8bc2cfbd3
Bhavsay Potan <bhavsaypotan@gmail.com>
2025-12-14 08:40:39 +0530

    Time: 14 ms (46.07%), Space: 43.8 MB (36.5%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -16,6 +16,42 @@ class Solution {
             map.put(sc,tc);
         }
         return true;
-       
     }
 }
+/*
+4️⃣ charMap.containsKey(sc)
+Checks whether this character from s is already mapped.Map = { e → a }
+sc = 'g'
+charMap.containsKey('g') → false
+Means:
+👉 'g' has no mapping yet
+
+Another example
+Map = { e → a, g → d }
+sc = 'g'
+charMap.containsKey('g') → true
+Means:
+👉 'g' is already mapped
+
+5️⃣ charMap.get(sc)
+Returns the mapped value for key sc.
+Map = { e → a, g → d }
+charMap.get('g') → 'd'
+if (charMap.get(sc) != tc)
+
+6️⃣ charMap.containsValue(tc)
+Checks whether this target character is already used by another key
+Why needed?
+To ensure one-to-one mapping.
+s = "ab"
+t = "aa"
+charMap.containsValue('a') → true
+
+7️⃣ charMap.put(sc, tc)
+👉 What it does
+Stores or updates a mapping.
+Example
+charMap.put('e', 'a');
+Map becomes:
+{ e → a }
+*/
