81cb262703a80db62c0286ce1419f1f5d156f014
Bhavsay Potan <bhavsaypotan@gmail.com>
2025-12-25 21:28:14 +0530

    Time: 13 ms (29.83%), Space: 46.5 MB (29.67%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,22 +1,20 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-
-        if(s.length()!=t.length()){
-            return false;
-        }
-        // anagram which was same letter and count of letter should be same.
+        if(s.length()!=t.length()) return false;
+        
         HashMap<Character,Integer> map1 = new HashMap<>();
         HashMap<Character,Integer> map2 = new HashMap<>();
 
         for(char sc : s.toCharArray()){
             map1.put(sc,map1.getOrDefault(sc,0)+1);
-        }
-
+        } 
         for(char tc : t.toCharArray()){
             map2.put(tc,map2.getOrDefault(tc,0)+1);
         }
 
         return map1.equals(map2);
+        
+        
     }
 }
 /*
