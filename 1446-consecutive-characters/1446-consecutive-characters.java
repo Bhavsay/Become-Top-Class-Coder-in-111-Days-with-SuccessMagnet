ebd1218fe6af8a967d4d756ad7dca73263727d49
Bhavsay Potan <bhavsaypotan@gmail.com>
2025-12-26 21:47:46 +0530

    Time: 1 ms (100%), Space: 43.5 MB (37.92%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.java
+++ 1446-consecutive-characters/1446-consecutive-characters.java
@@ -1,16 +1,31 @@
 class Solution {
     public int maxPower(String s) {
-        int max = 1; 
-        int count = 1; 
+        int max = 1;
+        int count = 1;
         for(int i=1; i<s.length(); i++){
             if(s.charAt(i)==s.charAt(i-1)){
                 count++;
-                max = Math.max(max,count);
+                max = Math.max(count,max);
+               
             }
-            else {
-                count = 1;
+            else{
+                count=1;
             }
         }
         return max;
     }
 }
+// public int maxPower(String s){
+//     int max = 1; 
+//     int count = 1;
+//     for(int i=1; i<s.length(); i++){
+//         if(s.charAt(i)==s.charAt(i-1)){
+//             count++;
+//             max = Math.max(max, count);
+//         }
+//         else{
+//             count=1;
+//         }
+//     }
+//     return max;
+// }
