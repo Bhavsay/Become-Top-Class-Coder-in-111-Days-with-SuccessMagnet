164c31329296b4e8ffc588e3c09d9604b17e25a8
Bhavsay Potan <bhavsaypotan@gmail.com>
2025-12-27 21:46:16 +0530

    Time: 0 ms (100%), Space: 43 MB (13.41%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,30 +1,43 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        HashMap<Character,String> map = new HashMap<>();
 
-        String[] words = s.split(" ");
-        if(pattern.length() != words.length){
+        String[] str = s.split(" ");
+
+        // length check
+        if (pattern.length() != str.length) {
             return false;
         }
 
-        for(int i=0; i<pattern.length(); i++){
-            char ch = pattern.charAt(i);
-            String str = words[i];
+        HashMap<Character, String> map = new HashMap<>();
 
-            if(map.containsKey(ch)){
-                if(!map.get(ch).equals(str)){
+        for (int i = 0; i < pattern.length(); i++) {
+            char ch = pattern.charAt(i);
+            String word = str[i];
+
+            if (map.containsKey(ch)) {
+                if (!map.get(ch).equals(word)) {
                     return false;
                 }
-            }
-            else{
-                if(map.containsValue(str)){
+            } else {
+                if (map.containsValue(word)) {
                     return false;
                 }
-                map.put(ch,str);
-               
+                map.put(ch, word);
             }
         }
         return true;
-        
     }
 }
+
+/*
+"dog cat cat dog" → ["dog","cat","cat","dog"]
+pattern = "aaa"
+words   = "aa aa aa aa"
+map.containsKey('a')  // true
+map.get('a') = "dog"
+str = "fish"
+if (!"dog".equals("fish")) {
+    return false;
+}
+*/
+
