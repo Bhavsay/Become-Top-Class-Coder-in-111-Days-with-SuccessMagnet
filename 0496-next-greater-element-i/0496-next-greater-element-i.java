b09d0644078707f33f8f67f060fca304a123864c
Bhavsay Potan <bhavsaypotan@gmail.com>
2026-01-05 12:03:34 +0530

    Time: 4 ms (73.05%), Space: 45.4 MB (21.74%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -5,34 +5,31 @@ class Solution {
 
         for(int i=nums2.length-1; i>=0; i--){
             int currentNum = nums2[i];
-
-            if(!map.isEmpty() && stack.peek()>currentNum){
-                map.put(currentNum, stack.peek());
-                
+            if(!stack.isEmpty() && stack.peek()>currentNum){
+                map.put(currentNum, stack.peek()); // { 2-7,
+                stack.push(currentNum); 
             }
             else if(stack.isEmpty()){
-                map.put(currentNum, -1);
-               
+                map.put(currentNum,-1);
+                stack.push(currentNum);
             }
             else {
                 while(!stack.isEmpty() && stack.peek()<currentNum){
                     stack.pop();
                 }
                 if(stack.isEmpty()){
-                    map.put(currentNum, -1);
-                    
+                    map.put(currentNum,-1);
+                    stack.push(currentNum);
                 }
                 else {
-                    map.put(currentNum, stack.peek());
-                    
+                    map.put(currentNum,stack.peek());
+                    stack.push(currentNum);
                 }
             }
-            stack.push(currentNum);
-
         }
-        int ans[] = new int[nums1.length];
+        int[] ans = new int[nums1.length];
         for(int i=0; i<nums1.length; i++){
-            ans[i] = map.get(nums1[i]);
+            ans[i]=map.get(nums1[i]);
         }
         return ans;
     }
