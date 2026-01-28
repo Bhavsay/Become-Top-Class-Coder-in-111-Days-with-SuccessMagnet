209c55589ad46e1d8e77c05352e53718ab8154d5
Bhavsay Potan <bhavsaypotan@gmail.com>
2026-01-28 23:19:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Search In Linked List/search-in-linked-list.java
@@ -0,0 +1,30 @@
+// User function Template for Java
+
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int data)
+    {
+        this.data = data;
+        this.next = null;
+    }
+}
+*/
+
+class Solution {
+    public static boolean searchLinkedList(Node head, int x) {
+        // Your code here
+        while(head != null){
+            if(head.data==x){
+                return true;
+                
+            }
+            head=head.next;
+        }
+        return false;
+        
+        
+    }
+}
