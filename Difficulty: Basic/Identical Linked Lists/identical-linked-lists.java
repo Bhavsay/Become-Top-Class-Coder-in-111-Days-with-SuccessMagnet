a9783257c13c18843c2930b02e2d130a7fa2c7c6
Bhavsay Potan <bhavsaypotan@gmail.com>
2026-01-29 21:16:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Identical Linked Lists/identical-linked-lists.java
@@ -0,0 +1,32 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}*/
+class Solution {
+    public boolean areIdentical(Node head1, Node head2) {
+        // code here
+     
+        while(head1!=null && head2!=null){
+            
+            if(head1.data!=head2.data){
+                return false;
+            }
+            head1=head1.next;
+            head2=head2.next;
+                
+        }
+        if(head1 == head2) {
+            return true;
+        }
+        return false;
+        
+        
+        
+    }
+}
