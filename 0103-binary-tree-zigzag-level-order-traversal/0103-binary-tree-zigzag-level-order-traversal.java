9b2693831065b29f34c81f7a319bdd1ec0e4d8a3
Bhavsay Potan <bhavsaypotan@gmail.com>
2026-04-24 22:15:57 +0530

    Time: 1 ms (77.21%), Space: 43.9 MB (11.05%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -15,36 +15,38 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-         List<List<Integer>> result = new ArrayList<>();
-
-        if (root == null) return result;
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null) return result;
 
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
 
         boolean leftToRight = true;
 
-        while (!queue.isEmpty()) {
-            int size = queue.size();
+        while(!queue.isEmpty()){
+            int levelSize = queue.size();
             LinkedList<Integer> level = new LinkedList<>();
-
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-
-                if (leftToRight) {
-                    level.addLast(node.val);
-                } else {
-                    level.addFirst(node.val);
+         
+            for(int i=0; i<levelSize; i++){
+                TreeNode currentNode = queue.poll();
+                
+                if(leftToRight){
+                    level.addLast(currentNode.val);
                 }
-
-                if (node.left != null) queue.offer(node.left);
-                if (node.right != null) queue.offer(node.right);
+                else {
+                    level.addFirst(currentNode.val);
+                }
+                if(currentNode.left != null ){
+                    queue.offer(currentNode.left);
+                }
+                if(currentNode.right != null ){
+                    queue.offer(currentNode.right);
+                }
             }
-
-            result.add(new ArrayList<>(level)); 
+            result.add(new ArrayList<>(level));
             leftToRight = !leftToRight;
         }
-
         return result;
+        
     }
 }
