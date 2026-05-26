4588072b1d60243a3483135b888db7b0affc38a3
Bhavsay Potan <bhavsaypotan@gmail.com>
2026-05-26 23:12:00 +0530

    Time: 2 ms (99.37%), Space: 119.2 MB (81.13%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,6 +1,5 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-
         int totalGas=0;
         for(int i:gas) totalGas+=i;
 
@@ -10,7 +9,6 @@ class Solution {
 
         if(totalGas < totalCost) return -1;
 
-        // Possible case
         int currGas=0;
         int start=0;
         for(int i=0;i<gas.length;i++){
