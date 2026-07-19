8b001d868ce8d5945fba877d6b4621b4d183ee66
Bhavsay Potan <bhavsaypotan@gmail.com>
2026-07-19 19:45:54 +0530

    Time: 1 ms (99.93%), Space: 46.2 MB (93.26%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,24 +1,26 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        
-        boolean[][] rows = new boolean[9][9];
-        boolean[][] cols = new boolean[9][9];
-        boolean[][] box  = new boolean[9][9];
+
+        boolean[][] row = new boolean[9][9];
+        boolean[][] col = new boolean[9][9];
+        boolean[][] box = new boolean[9][9];
 
         for(int i=0; i<9; i++){
             for(int j=0; j<9; j++){
                 if(board[i][j] != '.'){
                     int num = board[i][j]-'1';
                     int boxIdx = (i/3)*3+(j/3);
-                    if(rows[i][num] || cols[j][num] || box[boxIdx][num]){
+
+                    if(row[num][i] || col[num][j] || box[boxIdx][num]){
                         return false;
                     }
-                    rows[i][num] = cols[j][num] = box[boxIdx][num] = true;
+                    row[num][i] = col[num][j] = box[boxIdx][num] = true;
                 }
             }
-            
         }
         return true;
+
+
 
 
     }
