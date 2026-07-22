cef526c4283fb7cbbe4e00988e444002c70b96c5
Bhavsay Potan <bhavsaypotan@gmail.com>
2026-07-22 20:03:08 +0530

    Time: 5 ms (82.68%), Space: 77.5 MB (29.19%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,23 +1,20 @@
 class Solution {
     public int maxArea(int[] height) {
-        int maxWater = 0; // answer
-        int lp = 0, rp = height.length - 1; // left and right pointers
+        int lp = 0, rp = height.length-1, maxWater = 0;
+        while(lp<rp){
+            int w = rp - lp;
+            int ht = Math.min(height[lp], height[rp]);
+            int cw = w*ht;
 
-        while (lp < rp) {
-            int w = rp - lp; // width
-            int ht = Math.min(height[lp], height[rp]); // min height
-            int currWater = w * ht; // current area
+            maxWater = Math.max(maxWater, cw);
 
-            maxWater = Math.max(maxWater, currWater); // update max
-
-            // move the smaller pointer
-            if (height[lp] < height[rp]) {
+            if(height[lp]<height[rp]){
                 lp++;
             } else {
                 rp--;
             }
         }
-
         return maxWater;
     }
+
 }
