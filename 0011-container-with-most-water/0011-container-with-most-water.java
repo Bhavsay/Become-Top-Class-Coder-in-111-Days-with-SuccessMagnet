9d58f2621e80f16b3e27cffd8a5f155600365be4
Bhavsay Potan <bhavsaypotan@gmail.com>
2026-09-03 19:03:53 +0530

    Time: 5 ms (83.25%), Space: 77.5 MB (29.48%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,12 +1,13 @@
 class Solution {
     public int maxArea(int[] height) {
-        int maxWater = 0, lp = 0, rp = height.length-1;
+        int lp = 0, rp = height.length-1, maxWater = 0;
         while(lp<rp){
-            int w = rp  - lp;
+            int w = rp - lp;
             int ht = Math.min(height[lp], height[rp]);
             int cw = w*ht;
 
             maxWater = Math.max(maxWater, cw);
+
             if(height[lp]<height[rp]){
                 lp++;
             } else {
@@ -15,4 +16,5 @@ class Solution {
         }
         return maxWater;
     }
+
 }
