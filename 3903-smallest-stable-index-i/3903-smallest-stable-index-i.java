534872c4f1855ef593d886e8b8ceb0958cbf195b
Bhavsay Potan <bhavsaypotan@gmail.com>
2026-09-05 20:57:39 +0530

    Time: 1 ms (99.79%), Space: 46.4 MB (32.63%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -1,34 +1,85 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
 
-        int n = nums.length;
+        int n=nums.length; 
 
         int[] prefix = new int[n];
 
         prefix[0] = nums[0];
 
         for(int i=1; i<n; i++){
-            prefix[i] = Math.max(prefix[i-1],nums[i]);
-        } 
-        
-        int[] suffix = new int[n];
-
-        suffix[n-1]=nums[n-1];
-
-        for(int i = n-2; i>=0; i--){
-            suffix[i] = Math.min(suffix[i+1], nums[i]);
+            prefix[i] = Math.max(prefix[i-1], nums[i]);
         }
 
-        for(int i=0; i<n; i++){
-            int diff = prefix[i]-suffix[i];
-            if(diff<=k){
+        int[] suffix = new int[n];
+
+        suffix[n-1] = nums[n-1];
+
+        for(int i=n-2; i>=0; i--){
+            suffix[i] = Math.min(suffix[i+1],nums[i]);
+        }
+
+        for(int i=0; i<nums.length; i++){
+            int num = prefix[i]-suffix[i];
+            if(num<=k){
                 return i;
             }
         }
-
-
-
         return -1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // int n = nums.length;
+
+        // int[] prefix = new int[n];
+
+        // prefix[0] = nums[0];
+
+        // for(int i=1; i<n; i++){
+        //     prefix[i] = Math.max(prefix[i-1],nums[i]);
+        // } 
+        
+        // int[] suffix = new int[n];
+
+        // suffix[n-1]=nums[n-1];
+
+        // for(int i = n-2; i>=0; i--){
+        //     suffix[i] = Math.min(suffix[i+1], nums[i]);
+        // }
+
+        // for(int i=0; i<n; i++){
+        //     int diff = prefix[i]-suffix[i];
+        //     if(diff<=k){
+        //         return i;
+        //     }
+        // }
+
+
+
+        // return -1;
 
         
     }
