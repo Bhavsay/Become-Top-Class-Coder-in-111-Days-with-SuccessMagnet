0b662afb0e556edeea4bdb6f339d2221f2a5b129
Bhavsay Potan <bhavsaypotan@gmail.com>
2026-09-06 21:50:33 +0530

    Time: 0 ms (100%), Space: 41.9 MB (70.34%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,26 +1,31 @@
 class Solution {
-
-    // for DP learning, remember this pattern 
-    // Most Top-down DP / Memoization problems follow this structure:
-
     public int fib(int n) {
-        int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
+        
+        if(n==0) return 0;
+        if(n==1) return 1;
 
-        return helper(n,dp);
-    }
-    public int helper(int n, int[] dp){
+        int prev = 1;
+        int prev2 = 0;
 
-        // 1. Base case 
-        if(n<=1) return n;
-
-        // 2. Already calculated
-        if(dp[n] != -1){
-            return dp[n];
+        for(int i=2; i<=n; i++){
+            int next = prev + prev2;
+            prev2 = prev;
+            prev = next;
         }
+        return prev;
 
-        // 3. calculate + store
-        return dp[n] = helper(n-1, dp) + helper(n-2,dp);
+        // if(n==0) return 0;
+        // if(n==1) return 1;
+        // int prev = 1;
+        // int prev2 = 0;
+        // int curr = 0;
+        // for(int i=2; i<=n; i++){
+        //     curr = prev + prev2;
+        //     prev2 = prev;
+        //     prev = curr;
+        // }
+        // return prev;
 
     }
 }
+
