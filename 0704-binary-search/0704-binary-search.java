815870c70009ff131ace3ccd5217c1c546654ac9
Bhavsay Potan <bhavsaypotan@gmail.com>
2026-09-06 21:31:31 +0530

    Time: 0 ms (100%), Space: 48.4 MB (49.76%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,12 +1,10 @@
 class Solution {
     public int search(int[] nums, int target) {
 
-        int ans = Arrays.binarySearch(nums, target);
-
-        if (ans < 0) {
+        int res = Arrays.binarySearch(nums, target);
+        if(res<0){
             return -1;
         }
-
-        return ans;
+        return res;
     }
 }
