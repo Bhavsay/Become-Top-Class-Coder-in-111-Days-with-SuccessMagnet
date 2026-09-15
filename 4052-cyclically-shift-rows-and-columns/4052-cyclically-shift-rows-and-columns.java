4babc76e15b1750bc52f741acef0012ec558af1b
Bhavsay Potan <bhavsaypotan@gmail.com>
2026-09-15 23:14:54 +0530

    Time: 1 ms (100%), Space: 47.2 MB (95.3%) - LeetHub

--- 4052-cyclically-shift-rows-and-columns/4052-cyclically-shift-rows-and-columns.java
+++ 4052-cyclically-shift-rows-and-columns/4052-cyclically-shift-rows-and-columns.java
@@ -1,14 +1,29 @@
 class Solution {
     public int[][] cyclicShift(int n, int[][] grid, int[] rowShift, int[] colShift) {
 
-        int[][] result = new int[n][n];
-        for (int r = 0; r < n; r++)
-            for (int c = 0; c < n; c++) {
-                int nc = (c - rowShift[r] % n + n) % n;
-                int nr = (r - colShift[nc] % n + n) % n;
-                result[nr][nc] = grid[r][c];
+        // row shift
+        for(int i=0; i<n; i++){
+            int k = rowShift[i];
+            for(int x = 0; x < k; x++){
+                int first = grid[i][0];
+                for(int j=0; j<n-1; j++){
+                    grid[i][j] = grid[i][j+1];
+                }
+                grid[i][n-1] = first;
             }
-        return result;
+        }
 
+        // col shift
+        for(int j=0; j<n; j++){
+            int k = colShift[j];
+            for(int x=0; x<k; x++){
+                int first = grid[0][j];
+                for(int i=0; i<n-1; i++){
+                    grid[i][j] = grid[i+1][j];
+                }
+                grid[n-1][j] = first;
+            }
+        }
+        return grid;
     }
 }
