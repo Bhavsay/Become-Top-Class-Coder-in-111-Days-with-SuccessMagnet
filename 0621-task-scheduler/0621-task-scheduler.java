f7c1b9cb9dbb9f2ec250c270db51a9b69b38dc69
Bhavsay Potan <bhavsaypotan@gmail.com>
2026-09-16 23:59:59 +0530

    Time: 31 ms (25.43%), Space: 48.4 MB (22.31%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,17 +1,35 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
         int[] freq = new int[26];
-        for (char task : tasks) {
-            freq[task - 'A']++;
-        }
-        Arrays.sort(freq);
-        int chunk = freq[25] - 1;
-        int idle = chunk * n;
-
-        for (int i = 24; i >= 0; i--) {
-            idle -= Math.min(chunk, freq[i]);
+        for(int i=0; i<tasks.length; i++){
+            freq[tasks[i] - 'A']++;
         }
 
-        return idle < 0 ? tasks.length : tasks.length + idle;
+        //we want to execute highest freq tasks first and put into cooldown
+        PriorityQueue<Integer> maxH = new PriorityQueue<>(Collections.reverseOrder());
+        for(int count: freq){
+            if(count > 0)
+            maxH.offer(count);
+        }
+        //after executing a task put here
+        //[element, availableTime]
+        Queue<int[]> coolDown = new ArrayDeque<>();
+        int time = 0;
+        //if either has an element we still have to process
+        while(!maxH.isEmpty() || !coolDown.isEmpty()){
+            time++;
+            //imp: if cooldown is over add back to heap 
+            if(!coolDown.isEmpty() && coolDown.peek()[1] == time){
+                maxH.offer(coolDown.poll()[0]);
+            }
+            if(!maxH.isEmpty()){
+                int remaining = maxH.poll();
+                remaining--;
+                if(remaining > 0){
+                    coolDown.offer(new int[]{remaining, time+n+1});
+                }
+            }
+        }
+        return time;
     }
 }
