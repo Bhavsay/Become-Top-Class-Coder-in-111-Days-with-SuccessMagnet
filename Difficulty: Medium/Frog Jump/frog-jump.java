7b317e37d94640bb1a0947ae0fc1eb9166047551
Bhavsay Potan <bhavsaypotan@gmail.com>
2026-09-19 16:01:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Frog Jump/frog-jump.java
@@ -0,0 +1,27 @@
+class Solution {
+    int minCost(int[] height) {
+
+        if (height == null || height.length <= 1) return 0;
+
+        int n = height.length;
+
+        int[] dp = new int[n]; 
+
+        dp[0] = 0; 
+
+        for (int i = 1; i < n; i++) {
+
+            int left = dp[i - 1] + Math.abs(height[i] - height[i - 1]);
+
+            int right = Integer.MAX_VALUE;
+            if (i > 1) {
+                right = dp[i - 2] + Math.abs(height[i] - height[i - 2]);
+            }
+
+            dp[i] = Math.min(left, right);
+        }
+
+        return dp[n - 1];
+
+    }
+}
